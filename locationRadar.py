3ea67a986e3be482a6f87973c33cd2dfc368d883
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-11-22 11:54:18 +0200

    locationRadar V2: multiple locations, multiple keywords, save pkl&json

--- locationRadar.py
+++ locationRadar.py
@@ -8,14 +8,19 @@ __status__ = "Development"
 
 import sys
 import requests
-import csv
+import json
+import pickle
+from progressbar import ProgressBar, Percentage, Bar
 
 GOOGLE_PLACES_API_KEY = "AIzaSyAJkYNnUYUzOWmyN1qunzLBeroz5zeTDpE"
 SEARCH_RADIUS = 50000
 LOCATIONS = {
-    "Tel Aviv" : (32.088162,34.782470)
+    'North' : (32.781457, 35.309147),
+    'Tel Aviv' : (32.088162,34.782470),
+    'Beer Sheva' : (31.251320, 34.793456),
+    'South' : (29.797318, 34.939234),
 }
-SEARCH_KEYWORDS = [u'אולם_אירועים']
+SEARCH_KEYWORDS = [u'אולם_אירועים', u'אולם_חתונות', u'Wedding_Venue', u'Wedding_Hall']
 
 def main(argv):
     # Usage check
@@ -23,29 +28,49 @@ def main(argv):
         print("Usage: python %s" % argv[0])
         return -1
 
-    placesDict = {}
-    for location,coordinates in LOCATIONS.items():
+    fetchPlaces()
+
+
+def fetchPlaces():
+    placesDict = loadObj("placesDict")
+    print("Loaded {} places".format(len(placesDict)))
+
+    for location, coordinates in LOCATIONS.items():
         for keyword in SEARCH_KEYWORDS:
             searchResults = radarSearch(location, coordinates, SEARCH_RADIUS, keyword)
-            print("Found {} results".format(len(searchResults)))
+            if searchResults is None:
+                continue
 
+            print("Found {} results".format(len(searchResults)))
+            sys.stdout.flush()
+
+            progress = ProgressBar(widgets=[Percentage(), Bar()], max_value=len(searchResults), redirect_stdout=True)
+            counter = 0
             for searchResult in searchResults:
+                progress.update(counter)
+                counter += 1
+
                 placeId = searchResult['place_id']
                 if placeId not in placesDict:
                     placeDetails = getPlaceDetails(placeId)
-                    placesDict[placeId] = placeDetails
+                    if placeDetails is not None:
+                        placesDict[placeId] = placeDetails
+            progress.finish()
 
-    writePlacesToFile(placesDict)
+    print("Saving {} places".format(len(placesDict)))
+    saveObj(placesDict, "placesDict")
 
 def radarSearch(location, coordinates, radius, keyword):
     print("Searching {} in {}".format(keyword, location))
+    sys.stdout.flush()
+
     formatedCoordinates = "{},{}".format(coordinates[0], coordinates[1])
     response = requests.get(
         "https://maps.googleapis.com/maps/api/place/radarsearch/json?location={}&radius={}&keyword={}&key={}".format(
             formatedCoordinates, radius, keyword, GOOGLE_PLACES_API_KEY)).json()
 
     if response['status'] != 'OK':
-        print("Error accrued")
+        print("Error accrued: {}".format(response['status']))
         return
 
     return response['results']
@@ -53,15 +78,21 @@ def radarSearch(location, coordinates, radius, keyword):
 def getPlaceDetails(placeId):
     response = requests.get("https://maps.googleapis.com/maps/api/place/details/json?placeid={}&key={}".format(placeId, GOOGLE_PLACES_API_KEY)).json()
     if response['status'] != 'OK':
-        print("Error accrued")
+        print("Error accrued: {}".format(response['status']))
         return
     return response['result']
 
-def writePlacesToFile(placesDict):
-    with open('places.csv', 'w') as file:
-        writer = csv.writer(file, delimiter=',')
-        for key, value in placesDict.items():
-            writer.writerow([key, value])
+def saveObj(obj, name ):
+    with open('dataset/' + name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+    jsonDump = json.dumps(obj, ensure_ascii=False)
+    with open('dataset/' + name + '.json', 'wb') as file:
+        file.write(jsonDump.encode('utf-8'))
+
+def loadObj(name ):
+    with open('dataset/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 if __name__ == "__main__":
     main(sys.argv)
